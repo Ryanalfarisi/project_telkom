3673b7854613f23d3c87ea42cc295f35a58ed31d
ryan alfarisi <ryanalfarisi28@gmail.com>
2020-06-14 22:45:04 +0700

    Update minor

--- resources/views/lembur/edit.blade.php
+++ resources/views/lembur/edit.blade.php
@@ -235,7 +235,7 @@
           <label for="date" class="col-sm-4 col-form-label font-weight-bold">Date  <span class="cl-orange float-right">*</span></label>
           <div class="col-sm-8">
             <div class='input-group date' id='datetimepicker2'>
-                <input type='text' name="insert_date" id="currentData" value="{{$lembur->insert_date}}" class="form-control" placeholder="Pilih tanggal" required/>
+                <input type='text' name="insert_date" id="currentData" value="{{$lembur->insert_date}}" class="form-control" placeholder="{{$lembur->insert_date}}" required/>
                 <span class="input-group-addon">
                     <span class="glyphicon glyphicon-calendar"></span>
                 </span>
